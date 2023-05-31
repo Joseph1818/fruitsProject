ee59715867fa50168f4df834a82d670baf1d1ae8
Uncle Jee <ujee@UMBAS-MacBook-Air.local>
2023-05-31 16:14:48 +0200

    Second commit

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
-const { MongoClient } = require("mongodb");
-const assert = require("assert");
-
-// Connection URL
-const url = "mongodb://localhost:27017";
-// Database name
-const dbName = "fruitsDB";
-
-const client = new MongoClient(url, { useNewUrlParser: true });
-
-// use method to connect to the server
-
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("connected successfully to the server");
-  const db = client.db(dbName);
-
-  client.close();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {
+  useNewUrlParser: true,
 });
+
+// create a schema
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+// fruits collection
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty hard as fruit",
+});
+// to save the elements added in the fruits data dabe
+fruit.save();
+
+// Creating car schema and specification
+const CarSchema = new mongoose.Schema({
+  name: String,
+  model: String,
+  year: Number,
+  mileage: Number,
+});
+// New Model of the car
+
+const Car = mongoose.model("Car", CarSchema);
+
+const mercedes = new Car({
+  name: "mercedes",
+  model: "AMG",
+  year: 2022,
+  mileage: 80000,
+});
+
+const audi = new Car({
+  name: "audi",
+  model: "a4",
+  year: 2022,
+  mileage: 120000,
+});
+
+const bmw = new Car({
+  name: "Bmw",
+  model: "320i",
+  year: 2015,
+  mileage: 189000,
+});
+
+// This code save the collections into our database.
+Fruit.insertMany([mercedes, audi, bmw])
+  .then(function () {
+    console.log("All models of cars have been succesfully saved into the db");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
